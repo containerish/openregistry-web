9b80667315c925610e1b7fb9a4fb89703a6ac9c6
jay-dee7 <me@jsdp.dev>
2023-10-21 15:59:43 +0530

    feat: Add feature flags via PostHog
    Signed-off-by: jay-dee7 <me@jsdp.dev>

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,22 @@
-import type { HandleClientError } from '@sveltejs/kit';
+import { env } from "$env/dynamic/public";
+import type { HandleClientError } from "@sveltejs/kit";
+import posthog from "posthog-js";
+import { dev, browser } from "$app/environment";
+
+if (!dev && browser && env.PUBLIC_POSTHOG_API_KEY) {
+	posthog.init(env.PUBLIC_POSTHOG_API_KEY, {
+		api_host: env.PUBLIC_POSTHOG_API_HOST,
+	});
+}
 
 export const handleError: HandleClientError = async ({ error, event }) => {
-	console.log('unhandled client exception - route: %s - error: %s', event.route.id, error);
+	console.log(
+		"unhandled client exception - route: %s - error: %s",
+		event.route.id,
+		error
+	);
 
 	return {
-		message: typeof error === 'string' ? error : JSON.stringify(error)
+		message: typeof error === "string" ? error : JSON.stringify(error),
 	};
 };
